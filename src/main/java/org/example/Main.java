07fea1821c61100984cb484c56f7c50268a0bca2
Sercan Tuna <ssercantuna@hotmail.com>
2022-08-04 11:21:28 -0400

    done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import org.example.models.Capital;
+import org.example.models.Country;
+
 import javax.persistence.Persistence;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityManager;
@@ -14,6 +17,31 @@ public class Main {
         // access transaction object
         EntityTransaction transaction = entityManager.getTransaction();
 
+        Country italy = new Country();
+        italy.setName("Italy");
+
+        Country serbia = new Country();
+        serbia.setName("Serbia");
+
+        Capital rome = new Capital();
+        rome.setName("Rome");
+
+        Capital belgrade = new Capital();
+        belgrade.setName("Belgrade");
+
+        italy.setCapital(rome);
+        serbia.setCapital(belgrade);
+
+        transaction.begin();
+
+        entityManager.persist(italy);
+        entityManager.persist(serbia);
+
+        entityManager.persist(rome);
+        entityManager.persist(belgrade);
+
+
+        transaction.commit();
         // close entity manager
         entityManager.close();
         entityManagerFactory.close();
---
+++ src/main/java/org/example/models/Country.java
@@ -0,0 +1,25 @@
+package org.example.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+@Getter
+@Setter
+
+public class Country {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    private String name;
+
+    @OneToOne
+    private Capital capital;
+
+
+}
